a1eae85c93abc8cf71087b38fbc45d2a94d12d03
Vitor Silva <vitorsilva10413@gmail.com>
2023-02-12 18:14:34 +0000

    Removed SSL

--- src/server.js
+++ src/server.js
@@ -1,11 +1,4 @@
 const app = require('./app');
-const https = require('https');
-const fs = require('fs');
-
-const options = {
-    key: fs.readFileSync(`${__dirname}/private.key`),
-    cert: fs.readFileSync(`${__dirname}/certificate.pem`)
-};
 
 try {
     const user = {
@@ -23,12 +16,14 @@ try {
         nationality: 'Portuguese',
         phone: "+351 960123456"
     }
-    const result = await app.services.user.save(user);
-    if (result.message) throw app.services.user.update(1, user)
+    app.services.user.save(user)
+        .then((result) => {
+            if (result.message) throw app.services.user.update(1, user)
+        })
 } catch (err) {
     console.error(err)
 }
 
-https.createServer(options, app).listen(3001, () => {
+app.listen(3001, () => {
     console.log('Server started on port 3001');
 });
