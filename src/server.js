bbc03902f468f63593724552b62b2c16bf38e47a
Vitor Silva <vitorsilva10413@gmail.com>
2023-02-12 16:48:06 +0000

    Fixed some bugs

--- src/server.js
+++ src/server.js
@@ -7,6 +7,28 @@ const options = {
     cert: fs.readFileSync(`${__dirname}/certificate.pem`)
 };
 
+try {
+    const user = {
+        email: 'alguem@exemplo.com',
+        password: 'myP4s$W0rd',
+        full_name: `User For Test`,
+        birth_date: "2000/01/01",
+        address: {
+            street: "R. Exemplo n45",
+            city: "Lisboa",
+            state: "Lisboa",
+            zipCode: "4777-854"
+        },
+        country: 'Portugal',
+        nationality: 'Portuguese',
+        phone: "+351 960123456"
+    }
+    const result = await app.services.user.save(user);
+    if (result.message) throw app.services.user.update(1, user)
+} catch (err) {
+    console.error(err)
+}
+
 https.createServer(options, app).listen(3001, () => {
     console.log('Server started on port 3001');
 });
