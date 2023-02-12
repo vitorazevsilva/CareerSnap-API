2577c48990909ba3950f913fddff8de9d64dc869
Vitor Silva <vitorsilva10413@gmail.com>
2023-02-12 12:35:54 +0000

    HTTPS FIX

--- src/server.js
+++ src/server.js
@@ -1,3 +1,12 @@
 const app = require('./app');
+const https = require('https');
+const fs = require('fs');
 
-app.listen(3001);
+const options = {
+    key: fs.readFileSync(`${__dirname}/private.key`),
+    cert: fs.readFileSync(`${__dirname}/certificate.pem`)
+};
+
+https.createServer(options, app).listen(3001, () => {
+    console.log('Server started on port 3001');
+});
